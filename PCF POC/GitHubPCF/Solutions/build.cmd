08d8d1f018e3c11d53f5fd8e564e95c6f0a23ec4
Eickhel Mendoza <eickhelmc@intelequia.com>
2021-02-12 16:16:54 +0000

    Revised folder structure

--- PCF POC/GitHubPCF/Solutions/build.cmd
+++
@@ -1,7 +0,0 @@
-cd ..
-call npm run build
-cd Solutions
-rd bin /S /Q
-rd obj /S /Q
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe" /t:restore
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe"
